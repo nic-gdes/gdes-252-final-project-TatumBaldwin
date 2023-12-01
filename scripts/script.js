2ccfafe1895c7ec5a20bf72a5f24665fb02c617f
Tatum Baldwin <tjbaldwin@students.nic.edu>
2023-11-30 16:23:39 -0800

    Ham nav & fonts
    - created ham nav
    - uploaded Avenir

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// make hamburger menu icon turn into X and expand menu
+
+//grab the button and menu block
+const nav_btn = document.querySelector('.hamburger');
+const nav_menu = document.querySelector('.mobile-nav');
+
+// add classes to toggle
+nav_btn.addEventListener('click', function () {
+    nav_btn.classList.toggle('is-active');
+    nav_menu.classList.toggle('is-active');
+})
