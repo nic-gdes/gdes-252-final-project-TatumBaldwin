19c6eedb1bf0d37c74c502e4d8d602a1af52523d
Tatum Baldwin <tjbaldwin@students.nic.edu>
2023-12-02 20:12:24 -0800

    Calculator finished!!
    I created the calculator in another page because the other content was overwhelming me :)

--- scripts/script.js
+++ scripts/script.js
@@ -16,4 +16,41 @@ nav_btn.addEventListener('click', function () {
 
 //CALCULATOR
 
-//
+// Convert Fahrenheit (F) to Celsius (C)
+function convertFahr () {
+    //fetch value from input (parsInt makes the string a number)
+    const fer = parseInt(document.querySelector('#userInput').value);
+
+    // plug in Fahrenheit (F) to Celsius (C) conversion formula
+    const cels = (fer - 32) * 0.5556;
+    console.log(cels);
+
+    //display the number
+    document.getElementById('result').innerText = cels + " C";
+}
+
+// Convert Feet (ft) to Miles (mi)
+function convertFeet () {
+    //fetch value from input (parsInt makes the string a number)
+    const feet = parseInt(document.querySelector('#userInput').value);
+
+    //plug in Feet (ft) to Miles (mi) conversion formula
+    const miles = feet * 0.0001894;
+    console.log(miles);
+
+    //display the number
+    document.getElementById('result').innerText = miles + " mi";
+}
+
+// Convert Inches (in) to Centimeters (cm)
+function convertInch () {
+    //fetch value from input (parsInt makes the string a number)
+    const inch = parseInt(document.querySelector('#userInput').value);
+
+    //plug in Inches (in) to Centimeters (cm) conversion formula
+    const centimeters = inch * 2.54;
+    console.log(centimeters);
+
+    //display the number
+    document.getElementById('result').innerText = centimeters + " cm";
+}
