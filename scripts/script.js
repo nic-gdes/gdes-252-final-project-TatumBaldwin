14eea4493ed661a595bb3a6580d3ade7cc197dc3
Tatum Baldwin <tjbaldwin@students.nic.edu>
2023-12-07 14:12:51 -0800

    Color mode sucess

--- scripts/script.js
+++ scripts/script.js
@@ -65,34 +65,11 @@ function convertInch () {
 const darkMode = document.querySelector('.dark-light');
 //target the body
 const body = document.querySelector('#change-lighting');
-//target the text
-const text = document.querySelectorAll('p');
-const text2 = document.querySelectorAll('dl');
-//target nav
-const nav = document.querySelector('.flex-row');
-//target footer
-const footer = document.querySelector('.footer');
 
+// create button function
 darkMode.addEventListener('click', function () {
 
-    //change nav color 
-    nav.classList.toggle('nav-dark');
-
-    //change background color
-    body.classList.toggle('body-dark');
-
-    //change footer color
-    footer.classList.toggle('footer-dark');
-
-    //change text color
-    text[0].classList.toggle('text-dark');
-    text[1].classList.toggle('text-dark');
-    text[2].classList.toggle('text-dark');
-    text[3].classList.toggle('text-dark');
-    text[4].classList.toggle('text-dark');
-    text[5].classList.toggle('text-dark');
-
-    //change dl color
-    text2[0].classList.toggle('text-dark');
+    //change colors
+    body.classList.toggle('body');
 
 })
